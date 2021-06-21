255317d04c726f8a89b82f36d35bcc8e96c4ea68
Michael Jones <quixoticseven@gmail.com>
2021-06-21 02:00:51 -0700

    Added sound triggers while holding keys and midi out features.

--- corners.lua
+++ corners.lua
@@ -4,69 +4,52 @@ musicutil = require 'musicutil'
 engine.name = "MollyThePoly"
 
 Corners_Lib = include('lib/corners')
+Corners_Params = include('lib/corners_params')
 Engine_Bangs = include('lib/engine_bangs')
 Q7Util = include('lib/Q7Util')
 
 corners = Corners_Lib.new()
 bangs = Engine_Bangs.new()
 
-bangs.note_on = function (noteNumber, vel)
-    -- print("NoteOn "..noteNumber)
-    engine.noteOn(noteNumber, musicutil.note_num_to_freq(noteNumber), vel / 127.0)
-end
-
-bangs.note_off = function (noteNumber)
-    -- print("NoteOff")
-    engine.noteOff(noteNumber)
-end
-
-corners.bRight = function() bang_note(params:get("bRight")) end
-corners.bLeft = function() bang_note(params:get("bLeft")) end
-corners.bUp = function() bang_note(params:get("bUp")) end
-corners.bDown = function() bang_note(params:get("bDown")) end
-
 local g = grid.connect()
+local midi = midi.connect(1)
 
-local controlSpecs = {}
-
-controlSpecs.friction = controlspec.new(0, 1, 'lin', 0, 0.4, '%')
-controlSpecs.gravity = controlspec.new(0, 1, 'lin', 0, 0.98, '%')
-controlSpecs.noteLengthMin = controlspec.new(0, 16.0, 'lin', 0.01, 1/4, 'bt', 1/24/10)
-controlSpecs.noteLengthMax = controlspec.new(0, 16.0, 'lin', 0.01, 1/16, 'bt', 1/24/10)
+local heldKeyId = 1 -- polyphonic id for the held key
+local prevKeys = 0
+local defaultVel = 100
 
 function init()
     corners:bounds(g.cols, g.rows)
     -- corners:init()
-
-    params:add_separator()
-    params:add{type = "control", id = "friction", name = "Friction", controlspec = controlSpecs.friction}
-    params:add{type = "control", id = "gravity", name = "Gravity", controlspec = controlSpecs.gravity}
-
-    params:add_separator()
-    -- D4 A#4 D6 G6 - 62, 70, 86, 91
-    params:add{type="number", id="bRight", name="B Right", min = 0, max = 127, default = 62 }
-    params:add{type="number", id="bUp", name="B Up", min = 0, max = 127, default = 70 }
-    params:add{type="number", id="bLeft", name="B Left", min = 0, max = 127, default = 86 }
-    params:add{type="number", id="bDown", name="B Down", min = 0, max = 127, default = 91 }
-
-    params:add_separator()
-    params:add{type="number", id="velMin", name="Vel Min", min = 0, max = 127, default = 60 }
-    params:add{type="number", id="velMax", name="Vel Max", min = 0, max = 127, default = 120 }
-
-    params:add{type = "control", id = "noteLengthMin", name = "Length Min", controlspec = controlSpecs.noteLengthMin}
-    params:add{type = "control", id = "noteLengthMax", name = "Length Max", controlspec = controlSpecs.noteLengthMax}
     
-    params:add_separator()
+    Corners_Params.add_params()
     
     MollyThePoly.add_params()
     MollyThePoly.randomize_params("lead")
+    
+    corners.bRight = function() bang_note(params:get("bRight")) end
+    corners.bLeft = function() bang_note(params:get("bLeft")) end
+    corners.bUp = function() bang_note(params:get("bUp")) end
+    corners.bDown = function() bang_note(params:get("bDown")) end
+    
+    bangs.note_on = function (noteNumber, vel)
+        -- print("NoteOn "..noteNumber)
+        local channel = params:get("b_midiChannel")
+        note_on(noteNumber, vel, channel)
+    end
+    
+    bangs.note_off = function (noteNumber)
+        -- print("NoteOff")
+        local channel = params:get("b_midiChannel")
+        note_off(noteNumber, defaultVel, channel)
+    end
     
     clock.run(grid_redraw_clock) 
 end
 
 function key(n, v)
     -- corners:key(n, v)
-
+    
     if n == 2 and v == 1 then
         MollyThePoly.randomize_params("lead")
     elseif n == 3 and v == 1 then
@@ -96,6 +79,31 @@ function grid_redraw()
     corners:grr(params:get("gravity"))
     corners:bang()
     
+    -- play notes while holding keys
+    if corners.keys ~= prevKeys then
+        if corners.keys == 0 then
+            local channel = params:get("key_midiChannel")
+
+            local noteNumber = params:get(KEY_PARAMS[prevKeys])
+            note_off(noteNumber, defaultVel, channel)
+            prevKeys = corners.keys
+        else
+            if corners.keys <= #KEY_PARAMS then
+                local channel = params:get("key_midiChannel")
+                
+                if prevKeys ~= 0 then
+                    local prevNoteNumber = params:get(KEY_PARAMS[prevKeys])
+                    note_off(prevNoteNumber, defaultVel, channel)
+                end
+                local noteNumber = params:get(KEY_PARAMS[corners.keys])
+                note_on(noteNumber, defaultVel, channel)
+                prevKeys = corners.keys
+            end
+        end
+    end
+    
+    
+    
     g:all(0)
     
     corners:grid_redraw(g)
@@ -107,4 +115,30 @@ function bang_note(noteNumber)
     local lengthMin, lengthMax = Q7Util.get_min_max(params:get("noteLengthMin"), params:get("noteLengthMax"))
     local length = util.linlin(0,1, lengthMin, lengthMax, math.random())
     bangs:bang(noteNumber, 100, length)
+end
+
+function note_on(noteNumber, vel, channel)
+    vel = vel or defaultVel
+    channel = channel or 1
+    
+    if params:get("internalSynth") == 2 then
+        engine.noteOn(noteNumber, musicutil.note_num_to_freq(noteNumber), vel / 127.0)
+    end
+    
+    if params:get("midiOut") == 2 then
+        midi:note_on(noteNumber, vel, channel)
+    end
+end
+
+function note_off(noteNumber, vel, channel)
+    vel = vel or defaultVel
+    channel = channel or 1
+    
+    if params:get("internalSynth") == 2 then
+        engine.noteOff(noteNumber)
+    end
+    
+    if params:get("midiOut") == 2 then
+        midi:note_off(noteNumber, vel, channel)
+    end
 end
---
+++ lib/corners_params.lua
@@ -0,0 +1,55 @@
+local Corners_Params = {}
+Corners_Params.__index = Corners_Params
+
+local controlSpecs = {}
+
+controlSpecs.friction = controlspec.new(0, 1, 'lin', 0, 0.4, '%')
+controlSpecs.gravity = controlspec.new(0, 1, 'lin', 0, 0.98, '%')
+controlSpecs.noteLengthMin = controlspec.new(0, 16.0, 'lin', 0.01, 1/4, 'bt', 1/24/10)
+controlSpecs.noteLengthMax = controlspec.new(0, 16.0, 'lin', 0.01, 1/16, 'bt', 1/24/10)
+
+KEY_PARAMS = {"key1", "key2", "key3", "key4"}
+
+function Corners_Params.formatNote(param)
+    return musicutil.note_num_to_name(param:get(), true)
+end
+
+function Corners_Params.add_params()
+    params:add_separator()
+    params:add{type = "control", id = "friction", name = "Friction", controlspec = controlSpecs.friction}
+    params:add{type = "control", id = "gravity", name = "Gravity", controlspec = controlSpecs.gravity}
+    params:add_separator()
+    params:add{type = "option", id = "internalSynth", name = "Internal Synth", options = {"off", "on"}, default = 2, action = function(value)
+        if value ~= 2 then
+            engine.noteOffAll()
+        end
+    end}
+    params:add{type = "option", id = "midiOut", name = "Midi Out", options = {"off", "on"}, default = 1}
+    params:add{type = "number", id = "b_midiChannel", name = "B Channel", min = 1, max = 16, default = 1 }
+    params:add{type = "number", id = "key_midiChannel", name = "Key Channel", min = 1, max = 16, default = 2 }
+
+    params:add_separator()
+    -- D4 A#4 D6 G6 - 62, 70, 86, 91
+    params:add{type="number", id="bRight", name="B Right", min = 0, max = 127, default = 62, formatter = Corners_Params.formatNote }
+    params:add{type="number", id="bUp", name="B Up", min = 0, max = 127, default = 70, formatter = Corners_Params.formatNote }
+    params:add{type="number", id="bLeft", name="B Left", min = 0, max = 127, default = 86, formatter = Corners_Params.formatNote }
+    params:add{type="number", id="bDown", name="B Down", min = 0, max = 127, default = 91, formatter = Corners_Params.formatNote }
+
+    params:add_separator()
+
+    params:add{type="number", id=KEY_PARAMS[1], name="Key 1", min = 0, max = 127, default = 31, formatter = Corners_Params.formatNote }
+    params:add{type="number", id=KEY_PARAMS[2], name="Key 2", min = 0, max = 127, default = 34, formatter = Corners_Params.formatNote }
+    params:add{type="number", id=KEY_PARAMS[3], name="Key 3", min = 0, max = 127, default = 41, formatter = Corners_Params.formatNote }
+    params:add{type="number", id=KEY_PARAMS[4], name="Key 4", min = 0, max = 127, default = 50, formatter = Corners_Params.formatNote }
+
+    params:add_separator()
+    params:add{type="number", id="velMin", name="Vel Min", min = 0, max = 127, default = 60 }
+    params:add{type="number", id="velMax", name="Vel Max", min = 0, max = 127, default = 120 }
+
+    params:add{type = "control", id = "noteLengthMin", name = "Length Min", controlspec = controlSpecs.noteLengthMin}
+    params:add{type = "control", id = "noteLengthMax", name = "Length Max", controlspec = controlSpecs.noteLengthMax}
+    
+    params:add_separator()
+end
+
+return Corners_Params
